ce9c704fb1321c778e9cf55568e68eb36067eaed
akkun <akkun@akkunnoMacBook-Air.local>
2024-11-29 18:28:24 +0900

    change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,27 +7,105 @@ import { supabase } from "./supabase";
 
 const WordQuiz = () => {
   const [data, setData] = useState<Data[]>([]);
-  const [firstData, setFirstData] = useState<Data>({
-    id: "",
-    term: "",
-    meaning: "",
-    created_at: "",
-    learned_at: "",
-    tomorrow: false,
-    in_two_days: false,
-    in_three_days: false,
-  });
+  const [learnedData, setLearnedData] = useState<Data[]>([]);
+  const [firstData, setFirstData] = useState<Data>();
   const [clicked, setClicked] = useState(false);
 
-  //データ取得
   useEffect(() => {
-    const getAll = async () => {
+    const getData = async () => {
       const { data } = await supabase.from("wordsList").select("*");
-      setData(data || []);
-      setFirstData(data?.[data?.length - 1]);
+      if (data) {
+        const today = new Date() // 今日の日付
+          .toLocaleDateString("ja-JP", {
+            timeZone: "Asia/Tokyo",
+            year: "numeric", // 4桁で年を表示
+            month: "2-digit", // 月を2桁で表示
+            day: "2-digit", // 日を2桁で表示
+          })
+          .replace(/\//g, "-");
+
+        // 今日の `learned_at` のデータをフィルタリングして `Learning` にセット
+        const todayLearning = data.filter((item) => {
+          const learnedAt = new Date(item.learned_at); // learned_at を Date オブジェクトに変換
+          const learnedAtDate = learnedAt.toISOString().split("T")[0]; // YYYY-MM-DD 形式
+          return learnedAtDate === today; // 今日の日付と比較
+        });
+
+        setLearnedData(todayLearning); // 今日の学習データをセット
+
+        const selectedFields = data.map((item) => {
+          const typedItem = item as Data;
+
+          // 他のフィールドをフィルタリングして、tomorrowの値を更新
+          return Object.fromEntries(
+            Object.entries(typedItem).map(([key, value]) => {
+              if (value === true) {
+                // learned_atの日付を基に計算
+                const learnedAtDate = new Date(item.learned_at);
+
+                // keyによって異なる日数を加算
+                const targetDate = new Date(learnedAtDate);
+                if (key === "tomorrow") {
+                  targetDate.setDate(targetDate.getDate() + 1); // 翌日
+                } else if (key === "in_two_days") {
+                  targetDate.setDate(targetDate.getDate() + 2); // 2日後
+                } else if (key === "in_three_days") {
+                  targetDate.setDate(targetDate.getDate() + 3); // 3日後
+                }
+
+                // 新しい日付を"YYYY-MM-DD"形式で取得
+                const reviewDate = targetDate.toISOString().split("T")[0];
+
+                if (today < reviewDate) {
+                  return [key, reviewDate];
+                }
+              }
+
+              // 値がtrueでない場合はそのままkeyとvalueを返す
+              return [key, value];
+            })
+          );
+        }) as Data[];
+
+        const displayData: Data[] = selectedFields.filter((item) => {
+          // 1. itemの中でvalueがtrueのフィールドを探す
+          const hasTrueField = Object.values(item).includes(true);
+
+          // 2. itemに"tomorrow"や"in_two_days", "in_three_days"がすべてfalseであることをチェック
+          const hasAllFalseFields =
+            item.hasOwnProperty("tomorrow") &&
+            item.tomorrow === false &&
+            item.hasOwnProperty("in_two_days") &&
+            item.in_two_days === false &&
+            item.hasOwnProperty("in_three_days") &&
+            item.in_three_days === false;
+
+          // 条件を満たす場合はそのitemを返す
+          return hasTrueField || hasAllFalseFields;
+        });
+
+        displayData?.sort(
+          (a, b) =>
+            new Date(b.created_at).getTime() - new Date(a.created_at).getTime()
+        );
+
+        setData(displayData);
+      }
     };
-    getAll();
+    getData(); // データ取得関数を実行
   }, []);
+
+  useEffect(() => {
+    if (data.length > 0) setFirstData(data[0]);
+    sessionStorage.setItem("data", JSON.stringify(data));
+  }, [data]);
+
+  useEffect(() => {
+    if (data.length === 0) {
+      setFirstData(learnedData[0]);
+    }
+    sessionStorage.setItem("learnedData", JSON.stringify(learnedData));
+  }, [learnedData]);
 
   return (
     <main className="flex flex-col items-center fullscreen w-screen h-screen bg-blue-200 py-8">
@@ -37,37 +115,36 @@ const WordQuiz = () => {
       >
         リスト作成
       </Link>
-      {data.length > 0 && (
-        <section className="w-full h-full ">
-          <div className="mt-6 md:mt-10 flex justify-between items-center">
-            <h1 className="my-2 ml-6 md:ml-60 text-2xl md:text-4xl font-bold">
-              最新のリスト
-            </h1>
-            <Link
-              href="/wordQuiz/learnWord"
-              className="mr-10 md:mr-60 text-2xl md:text-4xl font-bold bg-green-400 text-white px-6 md:px-12 py-2 rounded-2xl"
-            >
-              学習
-            </Link>
-            <Link href="/wordQuiz/speech"></Link>
-          </div>
-          <div
-            onClick={() => setClicked(!clicked)}
-            className="w-4/5 md:w-3/5 h-1/4 md:h-1/2 bg-white rounded-2xl m-auto mt-4 md:mt-6 px-3 py-5"
+
+      <section className="w-full h-full ">
+        <div className="mt-6 md:mt-10 flex justify-between items-center">
+          <h1 className="my-2 ml-6 md:ml-60 text-2xl md:text-4xl font-bold">
+            最新のリスト
+          </h1>
+          <Link
+            href="/wordQuiz/learnWord"
+            className="mr-10 md:mr-60 text-2xl md:text-4xl font-bold bg-green-400 text-white px-6 md:px-12 py-2 rounded-2xl"
           >
-            <div className="h-4/5 flex items-center justify-center">
-              <h1 className="text-4xl md:text-6xl font-bold break-words">
-                {clicked ? firstData.meaning : firstData.term}
-              </h1>
-            </div>
-            <h3 className="w-full md:text-2xl text-center">
-              タップしたら
-              <span className="font-bold">{clicked ? "用語" : "意味"}</span>
-              を表示
-            </h3>
+            学習
+          </Link>
+          <Link href="/wordQuiz/speech"></Link>
+        </div>
+        <div
+          onClick={() => setClicked(!clicked)}
+          className="w-4/5 md:w-3/5 h-1/4 md:h-1/2 bg-white rounded-2xl m-auto mt-4 md:mt-6 px-3 py-5"
+        >
+          <div className="h-4/5 flex items-center justify-center">
+            <h1 className="text-4xl md:text-6xl font-bold break-words">
+              {clicked ? firstData?.meaning : firstData?.term}
+            </h1>
           </div>
-        </section>
-      )}
+          <h3 className="w-full md:text-2xl text-center">
+            タップしたら
+            <span className="font-bold">{clicked ? "用語" : "意味"}</span>
+            を表示
+          </h3>
+        </div>
+      </section>
     </main>
   );
 };
--- src/app/wordQuiz/learnWord/page.tsx
+++ src/app/wordQuiz/learnWord/page.tsx
@@ -10,77 +10,39 @@ import { GiSpeaker } from "react-icons/gi";
 const LearnWord = () => {
   // ステートの定義
   const [data, setData] = useState<Data[]>([]); // 単語データを保持
+  const [LearnedWords] = useState<Data[]>([]);
+  const [review, setReview] = useState<boolean>(false);
   const [index, setIndex] = useState<number>(0); // 現在の単語のインデックス
   const [term, setTerm] = useState<string>(""); // 現在の単語
   const [meaning, setMeaning] = useState<string>(""); // 現在の単語の意味
   const [reverse, setReverse] = useState<boolean>(false); // 表示を反転するフラグ
   const [SpeakerIsClicked, setSpeakerIsClicked] = useState<boolean>(false);
+  const [cardColor, setCardColor] = useState<string>(""); // 「わかった」「わからない」ボタンが押されたかどうかを管理する状態
 
-  // データ取得処理（初回レンダリング時のみ実行）
+  const getJapanTime = (): string => {
+    const date = new Date()
+      .toLocaleDateString("ja-JP", {
+        timeZone: "Asia/Tokyo",
+        year: "numeric", // 4桁で年を表示
+        month: "2-digit", // 月を2桁で表示
+        day: "2-digit", // 日を2桁で表示
+      })
+      .replace(/\//g, "-");
+    return date; // 例: "2024/11/27"
+  };
+
   useEffect(() => {
-    const getAll = async () => {
-      const { data } = await supabase.from("wordsList").select("*");
-      if (data) {
-        const selectedFields = data.map((item) => {
-          const typedItem = item as Data;
-
-          // 他のフィールドをフィルタリングして、tomorrowの値を更新
-          return Object.fromEntries(
-            Object.entries(typedItem).map(([key, value]) => {
-              if (value === true) {
-                // learned_atの日付を基に計算
-                const learnedAtDate = new Date(item.learned_at);
-
-                // keyによって異なる日数を加算
-                const targetDate = new Date(learnedAtDate);
-                if (key === "tomorrow") {
-                  targetDate.setDate(targetDate.getDate() + 1); // 翌日
-                } else if (key === "in_two_days") {
-                  targetDate.setDate(targetDate.getDate() + 2); // 2日後
-                } else if (key === "in_three_days") {
-                  targetDate.setDate(targetDate.getDate() + 3); // 3日後
-                }
-
-                // 新しい日付を"YYYY-MM-DD"形式で取得
-                const reviewDate = targetDate.toISOString().split("T")[0];
-                const today = getJapanTime();
-
-                if (today < reviewDate) {
-                  return [key, reviewDate];
-                }
-              }
-
-              // 値がtrueでない場合はそのままkeyとvalueを返す
-              return [key, value];
-            })
-          );
-        }) as Data[];
-
-        const displayData: Data[] = selectedFields.filter((item) => {
-          // 1. itemの中でvalueがtrueのフィールドを探す
-          const hasTrueField = Object.values(item).includes(true);
-
-          // 2. itemに"tomorrow"や"in_two_days", "in_three_days"がすべてfalseであることをチェック
-          const hasAllFalseFields =
-            item.hasOwnProperty("tomorrow") &&
-            item.tomorrow === false &&
-            item.hasOwnProperty("in_two_days") &&
-            item.in_two_days === false &&
-            item.hasOwnProperty("in_three_days") &&
-            item.in_three_days === false;
-
-          // 条件を満たす場合はそのitemを返す
-          return hasTrueField || hasAllFalseFields;
-        });
-
-        displayData?.sort(
-          (a, b) =>
-            new Date(b.created_at).getTime() - new Date(a.created_at).getTime()
-        );
-        setData(displayData);
-      }
-    };
-    getAll(); // データ取得関数を実行
+    // クライアントサイドでのみ実行
+    const storedData = JSON.parse(sessionStorage.getItem("data") || "[]");
+    if (storedData) {
+      setData(storedData);
+    }
+    const storedLearnedData = JSON.parse(
+      sessionStorage.getItem("learnedData") || "[]"
+    );
+    if (storedLearnedData) {
+      setData(storedLearnedData);
+    }
   }, []);
 
   // 現在の単語とその意味をステートに設定
@@ -92,35 +54,51 @@ const LearnWord = () => {
   // キーボード操作によるイベントハンドリング
   const handleKeyDown = (e: KeyboardEvent) => {
     switch (e.key) {
-      case "Enter": // Enterキーで表示反転
-      case "ArrowUp": // 上矢印キーで表示反転
-      case "ArrowDown": // 下矢印キーで表示反転
-        setReverse(!reverse); // 表示反転フラグを切り替え
-        break;
-      case "ArrowLeft": // 左矢印キーで前の単語へ
-        if (index > 0) {
-          setIndex((prevIndex) => prevIndex - 1);
-          setReverse(false); // 表示を元に戻す
+      case " ": // スペースキー
+        // スペースキーが押された時に表示反転フラグ（reverse）を切り替え
+        if (cardColor === "") {
+          setReverse(!reverse);
         }
         break;
-      case "ArrowRight": // 右矢印キーで次の単語へ
-        if (index < data.length - 1) {
-          setIndex((prevIndex) => prevIndex + 1);
-          setReverse(false); // 表示を元に戻す
+
+      case "s": // sキー
+        // sキーが押された時に音声を再生する処理（handleSpeak）を呼び出し
+        if (cardColor === "") {
+          handleSpeak();
         }
         break;
-      default: // 他のキーは何もしない
+
+      case "1": // 1キー
+        // 1キーが押された時に「理解した」とマークする処理（handleGotIt）を呼び出し
+        if (cardColor === "") {
+          handleGotIt();
+        }
+        break;
+
+      case "2": // 2キー
+        // 2キーが押された時に「理解していない」とマークする処理（handleNotGotIt）を呼び出し
+        if (cardColor === "") {
+          handleNotGotIt();
+        }
+        break;
+
+      default:
+        // 上記のいずれのキーも押されなかった場合、何もしない
         break;
     }
   };
 
   // キーボードイベントリスナーを登録・解除
   useEffect(() => {
-    window.addEventListener("keydown", handleKeyDown); // イベントリスナーを登録
+    // コンポーネントがマウントされたときに、windowオブジェクトにkeydownイベントリスナーを登録
+    window.addEventListener("keydown", handleKeyDown);
+
+    // クリーンアップ関数：コンポーネントがアンマウントされるか、依存が変更される時に
+    // イベントリスナーを解除して、メモリリークを防ぐ
     return () => {
-      window.removeEventListener("keydown", handleKeyDown); // クリーンアップ時に解除
+      window.removeEventListener("keydown", handleKeyDown); // イベントリスナーの解除
     };
-  });
+  }); // 空の依存配列なので、このeffectはコンポーネントがマウントされたときだけ実行
 
   // 読み上げを開始する関数
   const handleSpeak = () => {
@@ -137,58 +115,92 @@ const LearnWord = () => {
     }
   };
 
-  const getJapanTime = (): string => {
-    const date = new Date()
-      .toLocaleDateString("ja-JP", {
-        timeZone: "Asia/Tokyo",
-        year: "numeric", // 4桁で年を表示
-        month: "2-digit", // 月を2桁で表示
-        day: "2-digit", // 日を2桁で表示
-      })
-      .replace(/\//g, "-");
-    return date; // 例: "2024/11/27"
-  };
+  const handleGotIt = () => {
+    setCardColor("green");
+    setTimeout(async () => {
+      const today = getJapanTime(); // 日本時間（年月日）を取得
+      if (data) {
+        if (!review) {
+          const reviewDates = [
+            { name: "tomorrow", value: data[index].tomorrow },
+            { name: "in_two_days", value: data[index].in_two_days },
+            { name: "in_three_days", value: data[index].in_three_days },
+          ];
 
-  const handleGotIt = async () => {
-    const today = getJapanTime(); // 日本時間（年月日）を取得
+          const reviewDateIndex = reviewDates.findIndex(
+            (item) => item.value === true
+          );
+          if (reviewDateIndex === -1) {
+            reviewDates[0].value = true;
+          } else {
+            if (reviewDateIndex !== 2) {
+              reviewDates[reviewDateIndex].value = false;
+              reviewDates[reviewDateIndex + 1].value = true;
+            }
+          }
 
-    if (data) {
-      const reviewDates = [
-        { name: "tomorrow", value: data[index].tomorrow },
-        { name: "in_two_days", value: data[index].in_two_days },
-        { name: "in_three_days", value: data[index].in_three_days },
-      ];
+          const updateFields = {
+            learned_at: today, // learned_at を更新
+            tomorrow: reviewDates[0].value,
+            in_two_days: reviewDates[1].value,
+            in_three_days: reviewDates[2].value,
+          };
 
-      const reviewDateIndex = reviewDates.findIndex(
-        (item) => item.value === true
-      );
-      if (reviewDateIndex === -1) {
-        reviewDates[0].value = true;
-      } else {
-        if (reviewDateIndex !== 2) {
-          reviewDates[reviewDateIndex].value = false;
-          reviewDates[reviewDateIndex + 1].value = true;
+          // Supabaseにデータを保存
+          const { data: response, error } = await supabase
+            .from("wordsList")
+            .update(updateFields)
+            .eq("id", data[index].id)
+            .select();
+
+          if (error) {
+            console.error("Error saving date:", error.message);
+          } else {
+            console.log("Date saved successfully:", response);
+          }
+        }
+
+        if (index < data.length - 1) {
+          setIndex((prevIndex) => prevIndex + 1);
+          setReverse(false); // 表示を元に戻す
+        } else if (index === data.length - 1) {
+          setIndex(0);
+          setReverse(false);
+          setReview(false);
+          alert("すべて勉強しました");
+          setData([]);
         }
       }
+      setCardColor("");
+    }, 500);
+  };
 
-      const updateFields = {
-        learned_at: today, // learned_at を更新
-        tomorrow: reviewDates[0].value,
-        in_two_days: reviewDates[1].value,
-        in_three_days: reviewDates[2].value,
-      };
+  const handleNotGotIt = () => {
+    setCardColor("red");
+    setTimeout(async () => {
+      const today = getJapanTime(); // 日本時間（年月日）を取得
+      if (data) {
+        if (!review) {
+          const updateFields = {
+            learned_at: today, // learned_at を更新
+            tomorrow: true,
+            in_two_days: false,
+            in_three_days: false,
+          };
 
-      // Supabaseにデータを保存
-      const { data: response, error } = await supabase
-        .from("wordsList")
-        .update(updateFields)
-        .eq("id", data[index].id)
-        .select();
+          // Supabaseにデータを保存
+          const { data: response, error } = await supabase
+            .from("wordsList")
+            .update(updateFields)
+            .eq("id", data[index].id)
+            .select();
 
-      if (error) {
-        console.error("Error saving date:", error.message);
-      } else {
-        console.log("Date saved successfully:", response);
+          if (error) {
+            console.error("Error saving date:", error.message);
+          } else {
+            console.log("Date saved successfully:", response);
+          }
+        }
       }
 
       if (index < data.length - 1) {
@@ -196,40 +208,18 @@ const LearnWord = () => {
         setReverse(false); // 表示を元に戻す
       } else if (index === data.length - 1) {
         setIndex(0);
+        setReverse(false);
+        setReview(false);
         alert("すべて勉強しました");
+        setData([]);
       }
-    }
+      setCardColor("");
+    }, 500);
   };
 
-  const handleNotGotIt = async () => {
-    const today = getJapanTime(); // 日本時間（年月日）を取得
-    const updateFields = {
-      learned_at: today, // learned_at を更新
-      tomorrow: true,
-      in_two_days: false,
-      in_three_days: false,
-    };
-
-    // Supabaseにデータを保存
-    const { data: response, error } = await supabase
-      .from("wordsList")
-      .update(updateFields)
-      .eq("id", data[index].id)
-      .select();
-
-    if (error) {
-      console.error("Error saving date:", error.message);
-    } else {
-      console.log("Date saved successfully:", response);
-    }
-
-    if (index < data.length - 1) {
-      setIndex((prevIndex) => prevIndex + 1);
-      setReverse(false); // 表示を元に戻す
-    } else if (index === data.length - 1) {
-      setIndex(0);
-      alert("すべて勉強しました");
-    }
+  const handleReview = () => {
+    setData(LearnedWords);
+    setReview(true);
   };
 
   return (
@@ -244,47 +234,83 @@ const LearnWord = () => {
           戻る
         </Link>
         {/* ページタイトル */}
-        <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold text-black text-center mx-auto">
+        <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold mt-8 text-black text-center mx-auto">
           学習
         </h1>
       </section>
 
       {/* メインコンテンツ */}
       <section className="flex flex-col items-center justify-center w-full h-full mt-5 md:mt-10">
-        {/* 単語カード */}
-        <div className="relative w-4/5 md:w-3/5 h-[200px] sm:h-[300px] md:h-[400px] bg-white rounded-2xl shadow-lg flex items-center justify-center px-4 py-8">
-          {/* 単語または意味の表示 */}
-          <h1 className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl font-bold text-center">
-            {reverse ? meaning : term}
-          </h1>
-          {/* スピーカーアイコン */}
-          <div
-            onClick={handleSpeak}
-            className={`absolute top-4 right-4 text-4xl sm:text-5xl md:text-6xl cursor-pointer transition-all transform ${
-              SpeakerIsClicked ? "text-red-500" : "text-black"
-            } hover:text-red-500`}
-          >
-            <GiSpeaker />
+        {/* 今日のタスクが終わった場合 */}
+        {data.length === 0 ? (
+          <div className="flex flex-col items-center">
+            <h2 className="text-2xl sm:text-4xl md:text-5xl lg:text-6xl font-bold text-center mb-16 mt-12 md:mb-28 md:mt-24 text-red-500">
+              今日のタスクは終わりました！
+              <br />
+              お疲れ様です！
+            </h2>
+            <button
+              onClick={handleReview}
+              className="px-6 py-3 md:px-10 md:py-5 bg-blue-500 text-whit text-xl sm:text-3xl md:text-4xl lg:text-5xl text-white font-bold rounded-xl shadow-md hover:bg-blue-600 transition-colors"
+            >
+              復習しますか？
+            </button>
           </div>
-        </div>
+        ) : (
+          <>
+            {/* 単語カード */}
+            <div
+              onClick={() => {
+                if (cardColor === "") {
+                  setReverse(!reverse);
+                }
+              }}
+              className={`relative w-4/5 md:w-3/5 h-[200px] sm:h-[300px] md:h-[400px] rounded-2xl shadow-lg flex items-center justify-center px-4 py-8 transition-all duration-500 ${
+                cardColor === "green"
+                  ? "bg-green-300"
+                  : cardColor === "red"
+                  ? "bg-red-300"
+                  : "bg-white"
+              }`}
+            >
+              <h1 className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl font-bold text-center">
+                {reverse ? meaning : term}
+              </h1>
+              <div
+                onClick={handleSpeak}
+                className={`absolute top-4 right-4 text-4xl sm:text-5xl md:text-6xl cursor-pointer transition-all transform ${
+                  SpeakerIsClicked ? "text-red-500" : "text-black"
+                } hover:text-red-500`}
+              >
+                <GiSpeaker />
+              </div>
+            </div>
 
-        {/* ボタンセクション */}
-        <div className="flex w-4/5 md:w-2/3 lg:w-1/2 mt-8 sm:mt-12 md:mt-16 justify-between font-bold text-white text-sm md:text-xl lg:text-2xl mx-auto space-x-6 sm:space-x-10 md:space-x-14 lg:space-x-20">
-          {/* わかったボタン */}
-          <button
-            onClick={handleGotIt}
-            className="flex-1 px-6 py-4 bg-green-500 rounded-xl shadow-md hover:bg-green-600 transition-colors"
-          >
-            わかった
-          </button>
-          {/* わからないボタン */}
-          <button
-            onClick={handleNotGotIt}
-            className="flex-1 px-6 py-4 bg-red-500 rounded-xl shadow-md hover:bg-red-600 transition-colors"
-          >
-            わからない
-          </button>
-        </div>
+            {/* ボタンセクション */}
+            <div className="flex w-4/5 md:w-2/3 lg:w-1/2 mt-8 sm:mt-12 md:mt-16 justify-between font-bold text-white text-sm md:text-xl lg:text-2xl mx-auto space-x-6 sm:space-x-10 md:space-x-14 lg:space-x-20">
+              <button
+                onClick={() => {
+                  if (cardColor === "") {
+                    handleGotIt();
+                  }
+                }}
+                className="flex-1 px-6 py-4 bg-green-500 rounded-xl shadow-md hover:bg-green-600 transition-colors"
+              >
+                わかった
+              </button>
+              <button
+                onClick={() => {
+                  if (cardColor === "") {
+                    handleNotGotIt();
+                  }
+                }}
+                className="flex-1 px-6 py-4 bg-red-500 rounded-xl shadow-md hover:bg-red-600 transition-colors"
+              >
+                わからない
+              </button>
+            </div>
+          </>
+        )}
       </section>
     </main>
   );
